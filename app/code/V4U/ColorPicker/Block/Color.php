e92fb64b94287f11af343678b98e3712a470a171
Vrajesh Patel <vrajeshpatel4u@gmail.com>
2019-12-24 16:57:30 +0530

    Initial Commit Magento2 Color Picker

---
+++ app/code/V4U/ColorPicker/Block/Color.php
@@ -0,0 +1,42 @@
+<?php
+#File: app/code/Webspeaks/Color/Block/Color.php
+
+namespace V4U\ColorPicker\Block;
+
+class Color extends \Magento\Config\Block\System\Config\Form\Field {
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param Registry $coreRegistry
+     * @param array $data
+     */
+    public function __construct(
+    \Magento\Backend\Block\Template\Context $context, array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element) {
+        $html = $element->getElementHtml();
+        $value = $element->getData('value');
+
+        $html .= '<script type="text/javascript">
+            require(["jquery","jquery/colorpicker/js/colorpicker"], function ($) { 
+                $(document).ready(function () {
+                    var $el = $("#' . $element->getHtmlId() . '");
+                    $el.css("backgroundColor", "'. $value .'");
+
+                    // Attach the color picker
+                    $el.ColorPicker({
+                        color: "'. $value .'",
+                        onChange: function (hsb, hex, rgb) {
+                            $el.css("backgroundColor", "#" + hex).val("#" + hex);
+                        }
+                    });
+                });
+            });
+            </script>';
+        return $html;
+    }
+
+}
